592dab9efee1912fd2cca882e9f34d1ecacf60a4
Guilherme Vinhas <guilhermejosevinhas@gmail.com>
2023-06-23 00:25:46 -0300

    " Function productRegister complete "

--- Trabalho.cpp
+++ Trabalho.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <locale.h>
+#include <vector>
 
 using namespace std;
 
@@ -27,7 +28,7 @@ struct product
 	string descricao;
 	string modelo;
 	string marca;
-	string tamanha;
+	string tamanho;
 	string cor;
 	float preco;
 };
@@ -51,9 +52,21 @@ int userRegister(){
 	return 0;
 }
 
-int productRegister(){
-	cout << "Funcionou";
-	return 0;
+product productRegister(){
+	product Produto;
+	cout << "Qual a descrição do produto: ";
+	cin >> Produto.descricao;
+	cout << "Qual o modelo do produto: ";
+	cin >> Produto.modelo;
+	cout << "Qual a marca do produto: ";
+	cin >> Produto.marca;
+	cout << "Qual o tamanho do produto: ";
+	cin >> Produto.tamanho;
+	cout << "Qual a cor do produto: ";
+	cin >> Produto.cor;
+	cout << "Qual o preço do produto: ";
+	cin >> Produto.preco;
+	return Produto;
 }
 
 int productItemRegister(){
@@ -66,9 +79,8 @@ int sellsRegister(){
 	return 0;
 }
 
-int productsList(){
-	cout << "Funcionou";
-	return 0;
+void productsList(vector<product> productsVector){
+	cout << productsVector[0].marca;
 }
 
 int sellsList(){
@@ -78,16 +90,18 @@ int sellsList(){
 
 int main(){
 	int i;
+	vector<product> productsVector;
 	setlocale(LC_ALL,"Portuguese");
-	cout << "Menu de opï¿½ï¿½es" << endl;
+	cout << "Menu de opções" << endl;
 	cout << "1 - Cadastro de Pessoas \n";
 	cout << "2 - Cadastro de Produtos \n";
 	cout << "3 - Cadastro de Itens de Produtos \n";
 	cout << "4 - Cadastro de Venda \n";
 	cout << "5 - Listagem de produtos \n";
-	cout << "6 - Listagem das 10 ï¿½ltimas Vendas \n";
+	cout << "6 - Listagem das 10 últimas Vendas \n";
+	cout << "7 - Sair";
 	while (true){
-		cout << "\nQual funï¿½ï¿½o vocï¿½ deseja acessar? ";
+		cout << "\nQual função você deseja acessar? ";
 		cin >> i;
 		switch(i)
 		{
@@ -95,7 +109,7 @@ int main(){
 			userRegister();
 			break;
 		case 2:
-			productRegister();
+			productsVector.push_back(productRegister());
 			break;
 		case 3:
 			productItemRegister();
@@ -104,13 +118,15 @@ int main(){
 			sellsRegister();
 			break;
 		case 5:
-			productsList();
+			productsList(productsVector);
 			break;
 		case 6:
 			sellsRegister();
 			break;
+		case 7:
+			return 0;
 		default:
-			cout << "Opï¿½ï¿½o Invï¿½lida";
+			cout << "Opção Inválida";
 			break;
 		}
 		;}
