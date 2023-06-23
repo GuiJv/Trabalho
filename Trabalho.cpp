3cbd3ac32b38718f16bdd3a6c990b048558b6f85
Guilherme Vinhas <guilhermejosevinhas@gmail.com>
2023-06-23 14:11:24 -0300

    "userRegister, salesRegister, salesList func done"

--- Trabalho.cpp
+++ Trabalho.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-struct person
+struct user
 {
 	string nome;
-	int cpf;
+	double cpf;
 	string sexo;
 	string dataAniversario;
 	string endereco;
@@ -35,26 +35,78 @@ struct product
 
 struct itenProduct
 {
-	string product;
+	string produto;
 	string dataCompra;
 	float valorCompra;
 	string situacao;
 };
 
-struct sell
+struct sale
 {
-	string product;
+	string produto;
+	string cliente;
+	float valorVenda;
+	string dataVenda;
+	string dataRecebimento;
+	string situacao;
 };
 
+bool checkSN(char op){
+	if (toupper(op) == 'S'){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
 
-int userRegister(){
-	cout << "Funcionou";
-	return 0;
+
+user userRegister(){
+	user Pessoa;
+	char op;
+	cout << "Qual o nome da pessoa: ";
+	cin >> Pessoa.nome;
+	cout << "Qual o cpf da pessoa: ";
+	cin >> Pessoa.cpf;
+	cout << "Qual o sexo da pessoa: ";
+	cin >> Pessoa.sexo;
+	cout << "Qual a data de nascimento da pessoa: ";
+	cin >> Pessoa.dataAniversario;
+	cout << "Qual o time favorito da pessoa: ";
+	cin >> Pessoa.timeFavorito;
+	cout << "Qual a seleï¿½ï¿½o favorita da pessoa: ";
+	cin >> Pessoa.selecaoFavorita;
+	cout << "Gosta de Futebol:(S/N) ";
+	cin >> op;
+	Pessoa.gostaFutebol = checkSN(op);
+	cout << "Gosta de Volei:(S/N) ";
+	cin >> op;
+	Pessoa.gostaVolei = (checkSN(op));
+	cout << "Gosta de Basquete :(S/N) ";
+	cin >> op;
+	Pessoa.gostaBasquete = (checkSN(op));
+	cout << "Gosta de Handbol:(S/N) ";
+	cin >> op;
+	Pessoa.gostaHandbol = (checkSN(op));
+	cout << "Gosta de Nataï¿½ï¿½o:(S/N) ";
+	cin >> op;
+	Pessoa.gostaNatacao = (checkSN(op));
+	cout << "Gosta de Ciclismo:(S/N) ";
+	cin >> op;
+	Pessoa.gostaCiclismo = (checkSN(op));
+	cout << "Gosta de Atletismo:(S/N) ";
+	cin >> op;
+	Pessoa.gostaAtletismo = (checkSN(op));
+	cout << "Gosta de Musculaï¿½ï¿½o:(S/N) ";
+	cin >> op;
+	Pessoa.gostaMusculacao = (checkSN(op));
+	
+	return Pessoa;
 }
 
 product productRegister(){
 	product Produto;
-	cout << "Qual a descrição do produto: ";
+	cout << "Qual a descriï¿½ï¿½o do produto: ";
 	cin >> Produto.descricao;
 	cout << "Qual o modelo do produto: ";
 	cin >> Produto.modelo;
@@ -64,7 +116,7 @@ product productRegister(){
 	cin >> Produto.tamanho;
 	cout << "Qual a cor do produto: ";
 	cin >> Produto.cor;
-	cout << "Qual o preço do produto: ";
+	cout << "Qual o preï¿½o do produto: ";
 	cin >> Produto.preco;
 	return Produto;
 }
@@ -74,39 +126,69 @@ int productItemRegister(){
 	return 0;
 }
 
-int sellsRegister(){
-	cout << "Funcionou";
-	return 0;
+sale salesRegister(){
+	sale Venda;
+	cout << "Qual o produto: ";
+	cin >> Venda.produto;
+	cout << "Qual o cliente: ";
+	cin >> Venda.cliente;
+	cout << "Qual o valor da Venda: ";
+	cin >> Venda.valorVenda;
+	cout << "Qual a data da Venda: ";
+	cin >> Venda.dataVenda;
+	cout << "Qual a data de Recebimento: ";
+	cin >> Venda.dataRecebimento;
+	cout << "Qual a situaï¿½ï¿½o do produto: ";
+	cin >> Venda.situacao;
+	
+	return Venda;
 }
 
 void productsList(vector<product> productsVector){
-	cout << productsVector[0].marca;
+	for(int i = 0; i < productsVector.size(); i++){
+		cout << "\n" << "Descriï¿½ï¿½o: " << productsVector[i].descricao << "\n";
+		cout << "Modelo:    " << productsVector[i].modelo << "\n";
+		cout << "Marca:     " << productsVector[i].marca << "\n";
+		cout << "Tamanho:   " << productsVector[i].tamanho << "\n";
+		cout << "Cor:       " << productsVector[i].cor << "\n" ;
+		cout << "Preï¿½o:     " << productsVector[i].preco << "\n";
+	
+	}
 }
 
-int sellsList(){
-	cout << "Funcionou";
-	return 0;
+void sellsList(vector<sale> salesVector){
+	for(int i = 0; i < salesVector.size(); i++){
+		cout << "\n" << "Produto:             " << salesVector[i].produto << "\n";
+		cout << "Cliente:             " << salesVector[i].cliente << "\n";
+		cout << "Valor da Venda:      " << salesVector[i].valorVenda << "\n";
+		cout << "Data da Venda:       " << salesVector[i].dataVenda << "\n";
+		cout << "Data de Recebimento: " << salesVector[i].dataRecebimento << "\n";
+		cout << "SituaÃ§Ã£o:            " << salesVector[i].situacao << "\n";
+	}
+	cout << "\n";
 }
 
 int main(){
 	int i;
 	vector<product> productsVector;
+	vector<user> usersVector;
+	vector<sale> salesVector;
 	setlocale(LC_ALL,"Portuguese");
-	cout << "Menu de opções" << endl;
+	cout << "Menu de opï¿½ï¿½es" << endl;
 	cout << "1 - Cadastro de Pessoas \n";
 	cout << "2 - Cadastro de Produtos \n";
 	cout << "3 - Cadastro de Itens de Produtos \n";
 	cout << "4 - Cadastro de Venda \n";
 	cout << "5 - Listagem de produtos \n";
-	cout << "6 - Listagem das 10 últimas Vendas \n";
+	cout << "6 - Listagem das 10 ï¿½ltimas Vendas \n";
 	cout << "7 - Sair";
 	while (true){
-		cout << "\nQual função você deseja acessar? ";
+		cout << "\nQual funï¿½ï¿½o vocï¿½ deseja acessar? ";
 		cin >> i;
 		switch(i)
 		{
 		case 1:
-			userRegister();
+			usersVector.push_back(userRegister());
 			break;
 		case 2:
 			productsVector.push_back(productRegister());
@@ -115,18 +197,18 @@ int main(){
 			productItemRegister();
 			break;
 		case 4:
-			sellsRegister();
+			salesVector.push_back(salesRegister());
 			break;
 		case 5:
 			productsList(productsVector);
 			break;
 		case 6:
-			sellsRegister();
+			sellsList(salesVector);
 			break;
 		case 7:
 			return 0;
 		default:
-			cout << "Opção Inválida";
+			cout << "Opï¿½ï¿½o Invï¿½lida";
 			break;
 		}
 		;}
